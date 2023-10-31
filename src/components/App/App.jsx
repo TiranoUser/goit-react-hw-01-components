5fe9f8dbde2e8adb36b78e4d36141e52c8174975
TiranoUser <v130561@gmail.com>
2023-10-31 22:11:56 +0100

    edit 1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,12 @@
 import { Profile } from '../Profile/Profile';
-import user from '../../user.json';
+import user from '../../Data/user.json';
 import { GlobalStyle } from 'components/GlobalStyle';
 import { Statistics } from '../Statistics/Statistics';
-import data from '../../data.json';
+import data from '../../Data/data.json';
 import { FriendList } from 'components/FriendList/FriendList';
-import friends from '../../friends.json';
+import friends from '../../Data/friends.json';
 import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
-import transactions from '../../transactions.json';
+import transactions from '../../Data/transactions.json';
 
 export const App = () => {
   return (
